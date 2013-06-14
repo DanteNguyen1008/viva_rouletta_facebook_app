77599a1078e78d7ba325575b075558a0c9c501e8
Dante Nguyen <an.nguyenquocduy@gmail.com>
2013-06-14 13:45:22 +0700

    add database connection and upload form

--- User/upload_image_process.php
+++ User/upload_image_process.php
@@ -4,29 +4,30 @@ include ("database.php");
 if (isset($_POST['email'])) {
 
 	if ($_FILES['image_score']['name'] != NULL) {
-		$name = $_FILES['image_score']['name'];
-		move_uploaded_file($_FILES['image_score']['tmp_name'], "../upload/" . $name);
-		echo "<h1>Sent image successful</h1>";
-		echo "<img src='../upload/" . $name . "'>";
+		$filename = $_FILES['image_score']['name'];
+		$ext = substr($filename, strrpos($filename, '.') + 1);
+		if (($ext == "jpg" || $ext == "png") && ($_FILES["image_score"]["size"] < 100000)) {
+			move_uploaded_file($_FILES['image_score']['tmp_name'], "../upload/" . $filename);
+			echo "<h1>Sent image successful</h1>";
 
-		$url = '../upload/' . $name;
+			$url = '../upload/' . $name;
 
-		$username = "dante";
-		$passsword = "123456";
-		$nickname = "dante nguyen";
-		$birthdate = "1990-10-8";
-		$gender = "1";
-		$phone = "012345678";
-		$language = "vn";
-		$language = "vn";
-		$address = "vn";
+			$username = "dante";
+			$passsword = "123456";
+			$nickname = "dante nguyen";
+			$birthdate = "1990-10-8";
+			$gender = "1";
+			$phone = "012345678";
+			$language = "vn";
+			$language = "vn";
+			$address = "vn";
 
-		$db = new database();
-		$conn = $db -> connectToDB();
-		$email = trim($_POST['email']);
-		//insert to database
-		//insert user table
-		$sql = "INSERT INTO `kb_viva_rouletta_fb`.`user` ( 
+			$db = new database();
+			$conn = $db -> connectToDB();
+			$email = trim($_POST['email']);
+			//insert to database
+			//insert user table
+			$sql = "INSERT INTO `kb_viva_rouletta_fb`.`user` ( 
 				`username`, `email`, `password`, `nickname`, `birthday`, 
 				`gender`, `phonenumber`, `language`, `country`, `address`) 
 				VALUES ('" . $username . "', 
@@ -38,20 +39,22 @@ if (isset($_POST['email'])) {
 						'" . $language . "', 
 						'" . $address . "')";
 
-		$result = mysql_query($sql,$conn);
-		$new_user_id = mysql_insert_id();
+			$result = mysql_query($sql, $conn);
+			$new_user_id = mysql_insert_id();
+			//Insert image table
 
-		//Insert image table
-
-		$sql2 = "INSERT INTO `kb_viva_rouletta_fb`.`user_images` ( 
+			$sql2 = "INSERT INTO `kb_viva_rouletta_fb`.`user_images` ( 
 				`user_id`, `image_url`) 
 				VALUES ('" . $new_user_id . "', 
 						'" . $url . "')";
 
-		$result2 = mysql_query($sql2,$conn);
+			$result2 = mysql_query($sql2, $conn);
+		} else {
+			echo "<h1>Your image is not valid</h1>";
+		}
 
-		echo $result2;
-
+	}else{
+		echo "<h1>Your image is not valid</h1>"; 
 	}
 }
 ?>
